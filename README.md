f4540ef72907ddde0b80cd75905bdc82a870088e
Robson <128511701+rofersi@users.noreply.github.com>
2023-04-24 21:12:51 -0300

    Entrega desafio

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # desafio-dio-nome-ruby
 Desafio Ruby D.I.O
+
+Criar um programa na linguagem Ruby que dê a saída do
+nome complete do usuário e a sua idade
---
+++ app.rb
@@ -0,0 +1,7 @@
+puts "Informe seu nome completo: "
+nomeCompleto = gets.chomp
+
+puts "Informe sua idade: "
+idade = gets.chomp
+
+puts "Nome: #{nomeCompleto} - Idade: #{idade}"
